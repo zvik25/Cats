62260a97a7896ca332d2f4efb0363d70e75a3e36
zvik25 <zvik@mail.ru>
2025-07-29 20:01:39 +0300

    Добавлен список тегов

--- Cataas.py
+++ Cataas.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+Allowed_tags = ['sleep', 'jump', 'fight', 'black', 'white', 'bengal', 'siamese', 'cute', ]
 
 def load_image(url):
     try:
@@ -18,7 +21,7 @@ def load_image(url):
 
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_tag)
 
@@ -42,11 +45,8 @@ window.geometry('600x480')
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text='Загрузить по тегу', command=open_new_window)
-load_button.pack()
+# tag_entry = Entry()
+# tag_entry.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
@@ -57,8 +57,16 @@ file_menu.add_command(label='Загрузить фото', command=open_new_wind
 file_menu.add_separator()
 file_menu.add_command(label='Выход', command=exit)
 
-
 url = 'https://cataas.com/cat'
+
+tag_label = Label(text="Выберите тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text='Загрузить по тегу', command=open_new_window)
+load_button.pack()
 
 window.mainloop()
 

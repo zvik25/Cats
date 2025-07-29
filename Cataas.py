d9393445d837e1c64f6352fc2cbdff4bb92ac3c0
zvik25 <zvik@mail.ru>
2025-07-29 13:44:44 +0300

    Window and widjets are created

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,25 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title('Cats!')
+window.geometry('600x480')
+
+label = Label()
+label.pack()
+
+
+url = 'https://cataas.com/cat'
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+
+
+
+window.mainloop()
+

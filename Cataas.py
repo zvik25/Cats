492bebe444aa91aff1a27038ee68a098ad7adc97
zvik25 <zvik@mail.ru>
2025-07-29 14:05:59 +0300

    Size of image is align with a window size, button

--- Cataas.py
+++ Cataas.py
@@ -10,6 +10,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
+        img.thumbnail((600, 480), Image.Resampling.LANCZOS) # To have an image inside of window with button visible
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f'Произошла ошибка {e}')

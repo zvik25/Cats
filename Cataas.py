8e3cf30f67e36415140b05b264e4e678acd2ae95
zvik25 <zvik@mail.ru>
2025-07-29 13:58:36 +0300

    Button is added to upload a new image

--- Cataas.py
+++ Cataas.py
@@ -4,18 +4,24 @@ import requests
 from io import BytesIO
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
         return ImageTk.PhotoImage(img)
-    except Exceptionas e:
+    except Exception as e:
         print(f'Произошла ошибка {e}')
         return None
 
 
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
 
 
 window = Tk()
@@ -24,6 +30,9 @@ window.geometry('600x480')
 
 label = Label()
 label.pack()
+
+update_button = Button(text='Обновить', command=set_image)
+update_button.pack()
 
 
 url = 'https://cataas.com/cat'
@@ -34,7 +43,7 @@ if img:
     label.image = img
 
 
-
+set_image()
 
 window.mainloop()
 

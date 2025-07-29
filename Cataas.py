49236869edcb253ba55a877d60de58e321e70bd0
zvik25 <zvik@mail.ru>
2025-07-29 14:14:51 +0300

    Добавлено меню вместо кнопки

--- Cataas.py
+++ Cataas.py
@@ -25,6 +25,11 @@ def set_image():
         label.image = img
 
 
+def exit():
+    window.destroy()
+
+
+
 window = Tk()
 window.title('Cats!')
 window.geometry('600x480')
@@ -32,8 +37,17 @@ window.geometry('600x480')
 label = Label()
 label.pack()
 
-update_button = Button(text='Обновить', command=set_image)
-update_button.pack()
+# update_button = Button(text='Обновить', command=set_image)
+# update_button.pack()
+
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label='Выход', command=exit)
 
 
 url = 'https://cataas.com/cat'

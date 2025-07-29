3b9e03260aebb6eedaa017f44f36b96829e8c435
zvik25 <zvik@mail.ru>
2025-07-29 13:51:52 +0300

    Function load_image is created

--- Cataas.py
+++ Cataas.py
@@ -3,6 +3,21 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exceptionas e:
+        print(f'Произошла ошибка {e}')
+        return None
+
+
+
+
 window = Tk()
 window.title('Cats!')
 window.geometry('600x480')
